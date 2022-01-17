59680b2d1852bb44941c490a3ace9b8c35abd419
Louis Alary <louis.alary34@gmail.com>
2022-01-17 23:46:34 +0100

    Added routes to create a new offer and to save it

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\UtilisateurController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +21,21 @@ Route::get('/', [AccueilController::class,'index'])->name('accueil');
 Route::get('/offre/{id}', [AccueilController::class, 'show'])->name('offre.show');
 
 Route::get('/contact', [AccueilController::class, 'contact'])->name('contact');
+
+Route::get('/connexion', [UtilisateurController::class, 'connexion'])->name('connexion');
+
+Route::get('/inscription', [UtilisateurController::class, 'inscription'])->name('inscription');
+
+
+
+Route::group(['middleware' => ['auth']], function () { 
+    Route::get('/poster/offre', [UtilisateurController::class, 'poster'])->name('poster.create');
+    Route::post('/poster/offre', [UtilisateurController::class, 'enregistrer'])->name('poster.store');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
