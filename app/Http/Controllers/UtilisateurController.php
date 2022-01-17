cfc9c0155480726c211a872c2ebbae34aef7345a
Louis Alary <louis.alary34@gmail.com>
2022-01-17 23:45:33 +0100

    Added the view and the controller for an user to add a new offer

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offres;
+use App\Models\Type_maison;
 use Illuminate\Http\Request;
+
 
 class UtilisateurController extends Controller
 {
@@ -17,10 +20,31 @@ class UtilisateurController extends Controller
     }
 
     public function poster(){
-        return view('nouvelle_offre');
+
+        $types = Type_maison::all();
+
+        return view('nouvelle_offre',[
+            'types' => $types
+        ]);
     }
 
     public function enregistrer(Request $request){
+
+        Offres::create([
+            'titre' => $request->titre,
+            'type_habitation' => $request->type_hab,
+            'cout_loyer' => $request->cout_loyer,
+            'cout_charges' => $request->cout_charges,
+            'type_chauffage' => $request->type_chauf,
+            'superficie' => $request->superficie,
+            'description' => $request->description,
+            'adresse' => $request->adresse,
+            'ville' => $request->ville,
+            'code_postal' => $request->code_postal,
+        ]);
+
+        return view('dashboard');
+
         
     }
 
--- resources/views/nouvelle_offre.blade.php
+++ resources/views/nouvelle_offre.blade.php
@@ -2,21 +2,66 @@
 
 @section('content')
 
-    <form action="#" method="post">
+    <form action="{{ route('poster.store') }}" method="post">
 
     @csrf
-    <label for="name"> Titre :</label>
-    <input type="text" name="titre" class="border-4 border-sky-500">
 
-    <label for="name"> Prénom :</label>
-    <input type="text" name="prenom" class="border-4 border-sky-500">
+    
+    <label for="name"> Titre* :</label>
+    @if ($errors->has('titre')) <p class="help-block text-red-700">{{ $errors->first('titre') }}</p> @endif
+    <input type="text" name="titre" class="border-4 border-sky-500 block">
+    
+    <label for="name"> Type d'habitation* :</label>
+    <select name="type_hab" id="habitation" class="block">
 
-    <label for="name"> Adresse mail :</label>
-    <input type="text" name="adrmail" class="border-4 border-sky-500">
+    @foreach($types as $type)
+        <option value="{{ $type->description}} "> {{ $type->description}} </option>
+    @endforeach
 
-    <textarea name="content" cols="30" rows="10" class="border-4 border-sky-500"></textarea>
+    </select>
 
-    <button type="submit"> Envoyer</button>
+    
+    <label for="name"> Coût loyer :</label>
+    @if ($errors->has('cout_loyer')) <p class="help-block text-red-700">{{ $errors->first('cout_loyer') }}</p> @endif
+    <input type="text" name="cout_loyer" class="border-4 border-sky-500 block">
+
+    
+    <label for="name"> Coût charges* :</label>
+    @if ($errors->has('cout_charges')) <p class="help-block text-red-700">{{ $errors->first('cout_charges') }}</p> @endif
+    <input type="text" name="cout_charges" class="border-4 border-sky-500 block">
+
+    
+    <label for="name"> Type chauffage* : </label>
+    <input type="text" name="type_chauf" class="border-4 border-sky-500 block">
+
+
+
+    <label for="name"> Superficie* : </label>
+    @if ($errors->has('superficie')) <p class="help-block text-red-700">{{ $errors->first('superficie') }}</p> @endif
+    <input type="text" name="superficie" class="border-4 border-sky-500 block">
+
+    <label for="name"> Description* : </label>
+    @if ($errors->has('description')) <p class="help-block text-red-700">{{ $errors->first('description') }}</p> @endif
+    <textarea name="description" class="border-4 border-sky-500 block"> </textarea>
+
+
+    <label for="name"> Adresse* : </label>
+    @if ($errors->has('adresse')) <p class="help-block text-red-700">{{ $errors->first('adresse') }}</p> @endif
+    <input type="text" name="adresse" class="border-4 border-sky-500 block">
+
+    
+    <label for="name"> Ville : </label>
+    @if ($errors->has('ville')) <p class="help-block text-red-700">{{ $errors->first('ville') }}</p> @endif
+    <input type="text" name="ville" class="border-4 border-sky-500 block">
+
+    
+    <label for="name"> Code postal : </label>
+    @if ($errors->has('code_postal')) <p class="help-block text-red-700">{{ $errors->first('code_postal') }}</p> @endif
+    <input type="text" name="code_postal" class="border-4 border-sky-500 block">
+    
+    <button type="submit"> Poster votre offre </button>
+
+
     </form>
 
 @endsection
