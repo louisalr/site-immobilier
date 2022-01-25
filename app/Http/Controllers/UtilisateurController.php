89fe4eb15170d0dfad4a8945a151f56f5cf66349
Louis Alary <louis.alary34@gmail.com>
2022-01-25 23:44:38 +0100

    Added methods to add/remove favorites

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Offres;
+use App\Models\Favoris;
+use App\Models\Message;
 use App\Models\Type_maison;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 
 class UtilisateurController extends Controller
@@ -30,6 +34,19 @@ class UtilisateurController extends Controller
 
     public function enregistrer(Request $request){
 
+        /*
+
+        $request->validate([
+            'titre' => 'required',
+            'cout_loyer' => 'required | numeric',
+            'cout_charges' => 'required | numeric',
+            'superficie' => 'required | numeric',
+            'description' => 'required ',
+            'adresse' => 'required',
+            'ville' => 'required',
+            'code_postal' => 'required | min:5 | max:5'
+        ]);*/
+
         Offres::create([
             'titre' => $request->titre,
             'type_habitation' => $request->type_hab,
@@ -46,6 +63,71 @@ class UtilisateurController extends Controller
         return view('dashboard');
 
         
+    }
+
+    public function creer(){
+
+    }
+
+    public function envoyer(Request $request, $id){
+
+        
+        Message::create([
+            'contenu'-> $request->message,
+            'from_id' -> Auth::id(),
+            'to_id' -> $id,
+        ]);
+
+
+    }
+
+    public function favoris(){
+        return view('favoris');
+    }
+
+    public function ajouter_favoris() : JsonResponse
+    {
+        $offre = Offres::find(request()->id);
+
+        if($post->isFavoris()){
+            Favoris::where([
+                'user_id' => auth()->user()->id,
+                'post_id' => request()->id
+            ])->delete(); 
+
+            if($res){
+                return response()->json([
+                    'statut' => Offres::find(request()->id)->favoris->count()
+                ]);
+            }
+
+
+        }else{
+            $favori = new Favoris();
+
+            $favori->user_id = auth()->user()->id;
+            $favori->offre_id = request()->id;
+
+            $favori->save();
+
+            return response()->json([
+                'statut' => Offres::find(request()->id)->favoris->count()
+            ]);
+        }
+    }
+
+    public function AjouterFavoris(Offres $offre)
+    {
+        Auth::user()->favorites()->attach($offre->id);
+
+        return back();
+    }
+
+    public function RetirerFavoris(Offres $offre)
+    {
+        Auth::user()->favorites()->detach($offre->id);
+
+        return back();
     }
 
     
--- app/Models/Offres.php
+++ app/Models/Offres.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Message;
+
+use App\Models\Favoris;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +14,16 @@ class Offres extends Model
 
     protected $fillable = ['titre', 'type_habitation', 'cout_loyer', 'cout_charges', 'type_chauffage', 'superficie', 'description', 'adresse', 'ville', 'code_postal'];
 
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function favorited()
+    {
+        $id = auth()->user()->id;
+        return (bool) Favoris::where('user_id', $id)
+        ->where('offre_id', $this->id)
+        ->first();
+    }
 }
