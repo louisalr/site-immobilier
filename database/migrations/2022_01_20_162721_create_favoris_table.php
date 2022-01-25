8d6748ec16dcf5e26745490178b6b3820e95d5be
Louis Alary <louis.alary34@gmail.com>
2022-01-25 23:37:14 +0100

    Added table to save user's favorites

---
+++ database/migrations/2022_01_20_162721_create_favoris_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavorisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favoris', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+
+            $table->unsignedBigInteger('offre_id');
+            $table->foreign('offre_id')->references('id')->on('offres');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favoris');
+    }
+}
